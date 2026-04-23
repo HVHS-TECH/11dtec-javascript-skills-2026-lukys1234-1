419e4e6345d503158ac1ecf4e95eecb3b0d08a64
lukys1234 <24097lp@hvhs.school.nz>
2026-04-23 14:53:59 +1200

    TEST

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,9 @@
-/* Paste your code from task 1 here */
+/****************************
+This is a block comment.
+
+****************************/
+console.log("Running t01_introduction.js")
+console.log(/*CONNOR SG MNGRL MB yza!*/)
+
+// This is a single line comment
+
