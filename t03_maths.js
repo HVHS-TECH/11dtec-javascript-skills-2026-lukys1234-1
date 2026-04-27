85dc75f94616b5773f5d57c859feca8c49e265cb
lukys1234 <24097lp@hvhs.school.nz>
2026-04-28 10:59:32 +1200

    money

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,37 @@
-/* Paste your code from task 2 here */
+/****************************
+Name of Task:
+
+****************************/
+console.log("Running t01_introduction.js")
+console.log("CONNOR SG MNGRL MB yza!")
+
+// This is a single line comment
+
+//Variables 
+let name = "Jacob"
+let age = 13;
+let year = 2027;
+let money = 1000000;
+
+console.log("greetings " + name)
+
+console.log("You are " + age)
+
+console.log("The year is " + year)
+
+console.log("As of 2027, you have " + money)
+
+console.log("you spend half you money, you now have. " + money /2)
+
+
+
+/**********************************
+Main Code
+***************************************/
+
+
+
+
+/**************************************************
+Functions
+ ***************************************************/
