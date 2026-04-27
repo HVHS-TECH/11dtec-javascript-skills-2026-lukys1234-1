29a8ec27d6bd9e86849db55a4484623becd40d1b
lukys1234 <24097lp@hvhs.school.nz>
2026-04-28 10:48:46 +1200

    sadasdads

--- t02_variables.js
+++ t02_variables.js
@@ -8,6 +8,15 @@ console.log("CONNOR SG MNGRL MB yza!")
 // This is a single line comment
 
 //Variables 
+let name = "Jacob"
+let age = 13;
+let year = 2027;
+let money = 1000000;
+
+console.log("greetings " + name)
+console.log("You are " + age)
+console.log("The year is " + year)
+console.log("As of 2027, you have " + money)
 
 
 
